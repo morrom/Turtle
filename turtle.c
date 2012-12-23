4a8db4c5bc1756561eafa978ad7a4577e599b634
Fabio Z Tessitore <fabioztessitore@gmail.com>
2012-12-23 16:52:41 +0100

    reset

--- turtle.c
+++ turtle.c
@@ -1,40 +1,20 @@
-//turtle.c --> Programma Tartaruga
+/* turtle.c */
+
+/* Programma Tartaruga */
 
 #include <stdio.h>
 
-//struttura punti
-typedef struct {
-    int x,y;
-} _point;
-
-// struttura tartaruga
-typedef struct {
-    int pencildown;
-    _point position;
-} _turtle;
-
-// dichiarazione funzioni utilizzate
-//---------------------------------------
-_point np(int x,int y);
-void executeCommand (int cmd, int opt);
-void printFloor ();
-//---------------------------------------
-
-// variabili globali
-//---------------------------------------
-//matrice 50x50
-int matrix[50][50][1];
-_turtle turtle;
-//---------------------------------------
-
 int main()
 {
-    //inizializzazione floor a 0
-    int i,j;
-    for( i = 0; i<50; i++)
-        for( j = 0; j<50; j++)
-            matrix[i][j][0] = 0; // tutti i punti non sono disegnati
+  int floor[50][50];
 
-    //inizializzazione tartaruga
-    turtle.position=np(0,0);
-    turtle.pencildown=0;  // matita alzata
+  /* inizializzazione floor a 0 */
+  /* per inizializzare floor utilizziamo una funzione
+     es.
+
+     init_floor(???);
+
+  */
+
+  return 0;
+}
