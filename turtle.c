5d065af5b430ddf2af0a727b7feba36377cbcd22
morrom <ant.morra@gmail.com>
2012-12-26 14:27:19 +0100

    terza versione

--- turtle.c
+++ turtle.c
@@ -38,3 +38,20 @@ int main()
     //inizializzazione tartaruga
     turtle.position=np(0,0);
     turtle.pencildown=0;  // matita alzata
+
+    //comunicazione comandi alla tartaruga
+    executeCommand(2, 0);
+    executeCommand(6, 12);
+    executeCommand(3, 12);
+    executeCommand(5, 12);
+    executeCommand(4, 12);
+    executeCommand(1, 0);
+    executeCommand(7, 0);
+}
+
+_point np(int x,int y)
+{
+    _point aPoint={x,y}; //un nuovo punto viene creato di default "NON" disegnato
+    return aPoint;
+}
+
